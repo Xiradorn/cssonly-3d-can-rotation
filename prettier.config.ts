fb2cc50776133c21ff9d9802c4a119726eab97ba
Xiradorn <xiradorn@gmail.com>
2024-08-02 21:58:02 +0200

    :circus_tent: First commit

---
+++ prettier.config.ts
@@ -0,0 +1,10 @@
+import { Config } from "prettier";
+
+export default {
+	endOfLine: "auto",
+	semi: false,
+	trailingComma: "none",
+	useTabs: true,
+	tabWidth: 4,
+	htmlWhitespaceSensitivity: "ignore",
+} satisfies Config;
